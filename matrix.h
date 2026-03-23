95786dc531ef1a982edfdf167b4b72ed3a5a07db
mostly-working-code <88812142+iamnotgoodatprogrammingplshelp@users.noreply.github.com>
2026-03-23 23:09:54 +0200

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,1 +1,28 @@
+#ifndef MATRIX_H
+#define MATRIX_H
 
+#include <stddef.h>
+
+typedef struct {
+    size_t rows;
+    size_t cols;
+    double *data;
+} Matrix;
+
+Matrix *matrix_create(size_t rows, size_t cols);
+void matrix_free(Matrix *m);
+
+double matrix_get(const Matrix *m, size_t i, size_t j);
+void matrix_set(Matrix *m, size_t i, size_t j, double val);
+
+Matrix *matrix_add(const Matrix *a, const Matrix *b);
+Matrix *matrix_sub(const Matrix *a, const Matrix *b);
+Matrix *matrix_mul(const Matrix *a, const Matrix *b);
+Matrix *matrix_transpose(const Matrix *a);
+Matrix *matrix_identity(size_t n);
+
+double matrix_det(const Matrix *a);
+
+void matrix_print(const Matrix *m);
+
+#endif
